4497da6f139dd89cd06f9881d16f66a58ee45f23
gpcardonac <gpcardonac@gmail.com>
2023-03-14 13:44:46 -0500

    Crean componentes tareas y hojas de estilo para la caja que contenie las tareas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import WebDevelopment from './imagenes/WebDevelopment.jpg';
+import Tarea from './Componentes/Tarea';
 
 function App() {
   return (
     <div className="aplicacion tareas">
-      <div className='logo'>
+      <div className='logo-contenedor'>
         <img
           src={WebDevelopment}
           className='logo-desarrollo-web' />
-      
       </div>
+    <div className='tareas-lista-principal'>
+      <h1>Mis tareas</h1>
+      <Tarea texto='aprender react' />
     </div>
+  </div>   
+
+
   );
 }
 
