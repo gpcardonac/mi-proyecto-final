bb63d1906a0ad1b41a242db1a894dd3dbcd039ae
gpcardonac <gpcardonac@gmail.com>
2023-03-14 19:34:09 -0500

    Se almacenan las tareas se adiciona boton para eliminar tareas

--- src/Componentes/ListaDeTareas.js
+++ src/Componentes/ListaDeTareas.js
@@ -9,21 +9,33 @@ function ListaDeTareas() {
     const [tareas, setTareas] = useState([]);
 
     const agregarTarea = tarea => {
-        console.log("Tarea agregada");
-        console.log(tarea);
+
+
+        if (tarea.texto.trim()){
+            tarea.texto = tarea.texto.trim();
+
+            const tareasActualizadas = [tarea, ...tareas];
+            setTareas(tareasActualizadas);
+        }
 
     }
+        const eliminarTarea = id => {
+            const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+            setTareas(tareasActualizadas);
+        }
 
     return (
         <>
-            <TareaFormulario />
+            <TareaFormulario onSubmit={agregarTarea} />
             <div className='tareas-lista-contenedor'>
                 {
                    tareas.map((tarea) => 
                    <Tarea
-                   texto={tarea.tex}
+                   key={tarea.id}
+                   id={tarea.id}
+                   texto={tarea.texto}
                    completada={tarea.completada}
-                   />
+                   eliminarTarea={eliminarTarea} />
                    ) 
                 }
             </div>
--- src/Componentes/TareaFormulario.js
+++ src/Componentes/TareaFormulario.js
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
 import'../hojas-de-estilo/TareaFormulario.css';
+import {v4 as uuid4 } from 'uuid';
 
 function TareaFormulario(props) {
 
   const [input, setInput] = useState('');
     
   const manejarCambio = e => {
-    console.log('Escribiendo...');
+    setInput(e.target.value);
+   /* console.log('Escribiendo...');*/
+
   }  
 
   const manejarEnvio = e => {
+    e.preventDefault();
+  /*  console.log('enviando formulario...');*/
+
     const tareaNueva = {
-      id:'3454',
-      texto: 'hola'
+      id:uuid4(),
+      texto: input,
+      completada: false
     }
+
+    props.onSubmit(tareaNueva);
   }
 
     return (
-        <form className='tarea-formulario'>
+        <form 
+        className='tarea-formulario'
+        onSubmit={manejarEnvio}>
           <input
             className='tarea-input'
             type='text'
